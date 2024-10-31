2f6bdafb7dd3a48a5a3b634a4dea5f37df2babfd
Shagia <usenet@shagia.me>
2024-10-30 22:38:48 -0700

    Add config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://devin.sg',
+  integrations: [mdx(), react(), sitemap()],
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  api: "modern-compiler"
+              }
+          }
+      }
+  }
+});
