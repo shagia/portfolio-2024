5cb378131d79d76014657dccb3f7ea67f3caef06
Shagia <usenet@shagia.me>
2024-10-30 22:37:23 -0700

    Upload content bin

---
+++ src/content/config.ts
@@ -0,0 +1,42 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+// 2. Define your collection(s)
+const portfolioPostCollection = defineCollection({ 
+	type: 'content',
+	schema: ({image}) => z.object({
+		title: z.string(),
+		subtitle: z.string(),
+		description: z.string(),
+		tech: z.array(z.string()),
+		images: z.array(image()),
+	})
+});
+const wikiPostCollection = defineCollection({ 
+	type: 'content',
+	schema: ({image}) => z.object({
+		title: z.string(),
+		subtitle: z.string(),
+		asciiLogo: image(),
+		specs: z.array(z.string()),
+		description: z.string(),
+		images: z.array(image()),
+	})
+});
+const blogPostCollection = defineCollection({ 
+	type: 'content',
+	schema: ({image}) => z.object({
+		title: z.string(),
+		subtitle: z.string(),
+		description: z.string(),
+		images: z.array(image()),
+		featuredImage: image(),
+		tags: z.array(z.string())
+	})
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'wiki': wikiPostCollection,
+  'portfolio': portfolioPostCollection,
+  'blog': blogPostCollection
+};
